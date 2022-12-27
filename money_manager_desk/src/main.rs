1a387de829349be2b841e183823c1fcd764d22aa
ElrohirGT <elrohirgt@gmail.com>
2022-12-27 16:57:16 -0600

    fix: Styles not appearing on desktop version

--- money_manager_desk/src/main.rs
+++ money_manager_desk/src/main.rs
@@ -1,6 +1,13 @@
-use money_manager_common::app;
+use dioxus::prelude::*;
 
 fn main() {
     pretty_env_logger::init_timed();
     dioxus::desktop::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    rsx!(cx, div {
+        link { rel: "stylesheet", href: "https://unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css"}
+        money_manager_common::app{}
+    })
 }
